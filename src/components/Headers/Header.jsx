ee0576062f4ae53bc1826bad5126bbbdc6907903
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-07-04 22:29:21 +0300

    initial commit

--- src/components/Headers/Header.jsx
+++ src/components/Headers/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="header">
       <img src={Logo} alt="gym-logo" className="logo" />
-      <ul>
+      <ul className="header-menu">
         <li>Home</li>
         <li>Program</li>
         <li>Why us</li>
--- src/components/Heros/Hero.jsx
+++ src/components/Heros/Hero.jsx
@@ -6,6 +6,27 @@ export default function Hero() {
     <div className="hero">
       <div className="left-h">
         <Header />
+        {/*the-best-ad*/}
+        <div className="the-best-ad">
+          <div></div>
+          <span>the best club in the town</span>
+        </div>
+        {/* hero-heading*/}
+        <div className="hero-txt">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div>
+            <span>
+              Ideal body in here we will help you to shape and build your ideal
+              body and live up your life to fullest
+            </span>
+          </div>
+        </div>
       </div>
       <div className="right-h">right</div>
     </div>
