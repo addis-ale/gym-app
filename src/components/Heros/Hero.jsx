62fce750410bdc28fd66f637b21a438123826879
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-07-05 15:47:00 +0300

    initial commit

--- src/components/Heros/Hero.jsx
+++ src/components/Heros/Hero.jsx
@@ -59,6 +59,17 @@ export default function Hero() {
           <span>Heart Rate</span>
           <span>116 bpm</span>
         </div>
+        {/*hero image */}
+        <img src={hero_image} alt="" className="hero-image" />
+        <img src={hero_image_back} alt="" className="hero-image-back" />
+        {/* calories*/}
+        <div className="calories">
+          <img src={Calories} alt="" className="calorie-image" />
+          <div>
+            <span>Caloriesburned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
       </div>
     </div>
   );
