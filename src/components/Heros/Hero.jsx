5c069e1c4ff1cb2a7b6d73889abc72fa7159d916
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-07-05 14:57:02 +0300

    initial commit

--- src/components/Heros/Hero.jsx
+++ src/components/Heros/Hero.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Hero.css";
 import Header from "../Headers/Header";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
 export default function Hero() {
   return (
     <div className="hero">
@@ -33,11 +37,29 @@ export default function Hero() {
             <span>+140</span>
             <span>Expert coaches</span>
           </div>
-          <div></div>
-          <div></div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+        {/*hero buttons*/}
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
         </div>
       </div>
-      <div className="right-h">right</div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+      </div>
     </div>
   );
 }
