188c2a66debcacd06c807ce21effcd146657c364
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-07-07 20:18:08 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Hero from "./components/Heros/Hero";
 import Program from "./components/program/Program";
 import Reason from "./components/Reasons/Reason";
 import Plan from "./components/Plans/Plan";
+import Testimonial from "./components/Testimonial/Testimonial";
 
 import "./App.css";
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
       <Program />
       <Reason />
       <Plan />
+      <Testimonial />
     </div>
   );
 }
---
+++ src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,4 @@
+import "./Testimonial.css";
+export default function Testimonial() {
+  return <div className="testimonial"></div>;
+}
