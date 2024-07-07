e29d917a501d44c9800e89763d035e845f0b51d8
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-07-07 13:03:12 +0300

    initial commit

--- src/components/Reasons/Reason.jsx
+++ src/components/Reasons/Reason.jsx
@@ -5,6 +5,8 @@ import image3 from "../../assets/image3.png";
 import image4 from "../../assets/image4.png";
 import nike from "../../assets/nike.png";
 import tick from "../../assets/tick.png";
+import addidas from "../../assets/adidas.png";
+import newBalance from "../../assets/nb.png";
 export default function Reason() {
   return (
     <div className="reason" id="reasons">
@@ -16,7 +18,35 @@ export default function Reason() {
         <img src={image3} alt="" />
         <img src={image4} alt="" />
       </div>
-      <div className="right-r">right</div>
+      <div className="right-r">
+        <span>some reasons</span>
+        <div className="right-header">
+          <span className="stroke-text">why</span> choose us?<span></span>
+        </div>
+        <div className="why">
+          <div>
+            <img src={tick} alt="" />
+            <span>over 140+ expert coches</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />{" "}
+            <span> train smart and faster than before</span>
+          </div>
+          <div>
+            <img src={tick} alt="" /> <span>1 free program for new member</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span> reliable partners</span>
+          </div>
+        </div>
+        <span>our partners</span>
+        <div className="partners">
+          <img src={nike} alt="" />
+          <img src={addidas} alt="" />
+          <img src={newBalance} alt="" />
+        </div>
+      </div>
     </div>
   );
 }
