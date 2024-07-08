ea9e4a62e144935890bf2e1f9492ad8e73ad8e3d
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-07-08 20:36:09 +0300

    initial commit

--- src/components/Headers/Header.jsx
+++ src/components/Headers/Header.jsx
@@ -1,16 +1,42 @@
 import "./Header.css";
 import Logo from "../../assets/logo.png";
+import menu from "../../assets/bars.png";
+import { useState } from "react";
 export default function Header() {
+  const mobile = window.innerWidth <= 768;
+  const [isOpen, setIsOpen] = useState(false);
+  function handleMenu() {
+    console.log(`clicked`);
+    setIsOpen((isOpen) => !isOpen);
+  }
   return (
     <div className="header">
       <img src={Logo} alt="gym-logo" className="logo" />
-      <ul className="header-menu">
-        <li>Home</li>
-        <li>Program</li>
-        <li>Why us</li>
-        <li>Plans</li>
-        <li>Testimonials</li>
-      </ul>
+      {mobile ? (
+        <div className="menu-icon">
+          <img src={menu} alt=" menu" onClick={handleMenu} />
+        </div>
+      ) : (
+        <ul className="header-menu">
+          <li>Home</li>
+          <li>Program</li>
+          <li>Why us</li>
+          <li>Plans</li>
+          <li>Testimonials</li>
+        </ul>
+      )}
+      {isOpen && <Hamburger />}
     </div>
+  );
+}
+function Hamburger() {
+  return (
+    <ul className="hamburger">
+      <li>Home</li>
+      <li>Program</li>
+      <li>Why us</li>
+      <li>Plans</li>
+      <li>Testimonials</li>
+    </ul>
   );
 }
